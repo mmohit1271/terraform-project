86902683a567a437008f6e08ef583efd43b6fbb3
PRASHANT PRAKASH <155098714+prashant091993@users.noreply.github.com>
2025-01-05 11:20:13 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -1,6 +1,6 @@
 resource "aws_elb" "bar" {
   name               = "prashant-terraform-elb"
-  availability_zones = ["ap-southeast-1a", "ap-southeast-1b"]
+  availability_zones = ["ap-south-1a", "ap-south-1b"]
 
   listener {
     instance_port     = 80
@@ -39,7 +39,7 @@ resource "aws_elb" "bar" {
 
 /*resource "aws_instance" "one" {
   for_each = toset(["one", "two", "three"])
-  ami           = "ami-006be9ab6a140de6e"
+  ami           = "ami-0fd05997b4dff7aac"
   instance_type = "t2.micro"
   tags = {
     Name = "instance-${each.key}"
