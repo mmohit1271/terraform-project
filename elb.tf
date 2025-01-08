dd2ad9df673b1253f51f46ebe1027ccf8f14286e
Mohit Mishra <99587336+mmohit1271@users.noreply.github.com>
2025-01-08 22:33:20 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -1,6 +1,6 @@
 resource "aws_elb" "bar" {
   name               = "mmishra-terraform-elb"
-  availability_zones = ["ap-south-1a", "ap-south-1b"]
+  availability_zones = ["eu-west-1a", "eu-west-1b"]
 
   listener {
     instance_port     = 80
@@ -21,7 +21,7 @@ resource "aws_elb" "bar" {
   cross_zone_load_balancing = true
   idle_timeout              = 400
   tags = {
-    Name = "raham-tf-elb"
+    Name = "mohit-tf-elb"
   }
 }
 
